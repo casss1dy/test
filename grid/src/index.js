2e60186dd9b0bde15de2c78f573afce940108573
Nadya Khramova <casssidy@ya.ru>
2020-02-13 02:59:42 +0400

    feat: add modal edit form layout

--- grid/src/index.js
+++ grid/src/index.js
@@ -2,4 +2,12 @@ import './style.css';
 
 $(document).ready(function(){
 
+  $('.btn-edit').on('click', toggleModal);
+  $('.modal-edit .close').on('click', toggleModal);
+
+  function toggleModal() {
+    $('body').toggleClass('backdrop');
+    $('.modal-edit').fadeToggle();
+  }
+
 });
